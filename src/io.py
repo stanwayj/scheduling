edfa71a812d7cec05f29c6d2c84fbf3461ed4b94
Josh Stanway <j.stanway19@gmail.com>
2026-08-24 12:15:57 -1000

    Load and sort observations into a DataFrame.

---
+++ src/io.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import csv
+
+# From https://stackoverflow.com/a/69796836
+def get_delimiter(file_path, bytes = 4096):
+    sniffer = csv.Sniffer()
+    data = open(file_path, "r").read(bytes)
+    delimiter = sniffer.sniff(data).delimiter
+    return delimiter
+
+def load_csv(fname):
+
+    delimiter = get_delimiter(fname)
+    df = pd.read_csv(fname, sep=delimiter)
+
+    # Remove planets
+    df = df[df.coordstype != 'PLANET']
+
+    # Convert RA and DEC to degrees
+    df['ra2000'] = np.rad2deg(df['ra2000'])
+    df['dec2000'] = np.rad2deg(df['dec2000'])
+
+    # Sort array by instrument and priority
+    df = df.sort_values(by=['instrument', 'tagpriority'])
+
+    return df
