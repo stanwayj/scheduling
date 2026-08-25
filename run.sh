96b024ca4ef1c3005f20ef9d0ab2da750376a5a6
Josh Stanway <j.stanway19@gmail.com>
2026-08-25 12:23:17 -1000

    Ensure the first commadn line argument is either clean or a configuration file.

--- run.sh
+++ run.sh
@@ -4,20 +4,21 @@
 # 1. make the argument accept parameter file
 # 2. explain different arguments
 
-# Load argument from command line argument
-argument=$1
+# Load arguments from command line argument
+option1=$1
 
-# If argument is clean, clean data and plots, then kill operation
-if [[ "$argument" == "clean" ]]; then
+# Option must either be clean, or a configuration file
+# If option1 is clean, clean data and plots, then kill operation
+if [[ "$option1" == "clean" ]]; then
     echo "Cleaning data and plot directories"
     rm "./plots"/*
     rm "./data_in"/*
     rm "./data_out"/*
     exit 1
+elif [[ "$option1" == *.yaml ]]; then
+    echo "Plotting summary and running schedule from $option1"
+    python3 ./src/plot_summary.py "$option1"  
+    python3 ./src/schedule.py "$option1"  
+else
+    echo "The first command line argument must be ``clean'' or a yaml configuration file"
 fi
-
-# Plot basic summary of observations
-python3 ./src/plot_summary.py "$argument"
-
-# Run schedule
-python3 ./src/schedule.py "$argument"
